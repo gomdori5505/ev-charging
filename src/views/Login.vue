b1c7a122735bc1b2968af8157ba3de168b42275c
lsk <lsk@lskui-MacBookPro.local>
2021-01-15 20:16:24 +0900

    feat: add email and password rule

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,27 +13,38 @@
             <v-toolbar-title>로그인</v-toolbar-title>
           </v-toolbar>
           <div class="pa-3">
-            <v-text-field
-              v-model="email"
-              label="이메일을 입력하세요."
-            ></v-text-field>
-            <v-text-field
-              v-model="password"
-              type="password"
-              label="패스워드를 입력하세요."
-            ></v-text-field>
-            <v-btn
-              color="primary"
-              depressed
-              block
-              large
-              @click="login({
-                email,
-                password
-              })"
+            <v-form
+              ref="form"
+              v-model="valid"
+              lazy-validation
             >
-              로그인
-            </v-btn>
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                label="E-mail을 입력하세요."
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                :rules="passwordRules"
+                type="password"
+                label="Password를 입력하세요."
+                required
+              ></v-text-field>
+              <v-btn
+                :disabled="!valid"
+                color="primary"
+                depressed
+                block
+                large
+                @click="login({
+                  email,
+                  password
+                })"
+              >
+                로그인
+              </v-btn>
+            </v-form>
           </div>
         </v-card>
       </v-flex>
@@ -46,25 +57,35 @@ export default {
   name: 'Login',
   data() {
     return {
+      valid: true,
       email: null,
+      emailRules: [
+        v => !!v || 'E-mail은 필수입니다.',
+        v => /.+@.+\..+/.test(v) || 'E-mail이 유효하지 않습니다.'
+      ],
       password: null,
+      passwordRules: [
+        v => !!v || 'Password는 필수입니다.'
+      ],
       inCorrectAccount: false
     }
   },
   methods: {
     login() {
-      this.$firebase.auth().signInWithEmailAndPassword(this.email, this.password)
-        .then((user) => {
-          this.$store.dispatch('snackBarOpen', '로그인 되었습니다.')
-          this.$router.push({
-            name: 'home'
+      if(this.$refs.form.validate()) {
+        this.$firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+          .then((user) => {
+            this.$store.dispatch('snackBarOpen', '로그인 되었습니다.')
+            this.$router.push({
+              name: 'home'
+            })
           })
-        })
-        .catch((error) => {
-          console.log(error.code)
-          console.log(error.message)
-          this.inCorrectAccount = true
-        });
+          .catch((error) => {
+            console.log(error.code)
+            console.log(error.message)
+            this.inCorrectAccount = true
+          });
+      }
     },
   },
 }

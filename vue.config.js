8aa2f42cf28272fef1367e63318aedb86bb3f2a4
lsk <lsk@lskui-MacBookPro.local>
2021-01-20 23:30:50 +0900

    fix: remove build

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,5 @@ module.exports = {
   devServer: {
     proxy: 'http://open.ev.or.kr:8080'
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/ev-charging/'
-    : '/'
+  publicPath: '/'
 }
